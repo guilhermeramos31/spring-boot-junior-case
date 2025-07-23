d6a51a6b6dcdbf1bf1b6b7a98bad87b39349ec04
guilhermeramos31 <guilhermeramos31.gr@gmail.com>
2025-07-23 17:57:00 -0300

    feat: model of books and relationships between created entities

--- src/main/java/com/guilhermeramos31/springbootjuniorcase/model/category/Category.java
+++ src/main/java/com/guilhermeramos31/springbootjuniorcase/model/category/Category.java
@@ -1,11 +1,17 @@
 package com.guilhermeramos31.springbootjuniorcase.model.category;
 
 import com.guilhermeramos31.springbootjuniorcase.model.base.entity.BaseEntity;
+import com.guilhermeramos31.springbootjuniorcase.model.book.Book;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -15,4 +21,7 @@ import lombok.Setter;
 public class Category extends BaseEntity {
     private String name;
     private String description;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Book> books = new HashSet<>();
 }
