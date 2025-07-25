350e42bd384adc380468489156959bc4f5b5f7d0
guilhermeramos31 <guilhermeramos31.gr@gmail.com>
2025-07-25 15:30:40 -0300

    doc: swagger annotation to category controller

--- src/main/java/com/guilhermeramos31/springbootjuniorcase/controller/CategoryController.java
+++ src/main/java/com/guilhermeramos31/springbootjuniorcase/controller/CategoryController.java
@@ -7,6 +7,8 @@ import com.guilhermeramos31.springbootjuniorcase.model.category.dto.CategoryRequ
 import com.guilhermeramos31.springbootjuniorcase.model.category.dto.CategoryResponseDTO;
 import com.guilhermeramos31.springbootjuniorcase.services.category.interfaces.CategoryService;
 import com.guilhermeramos31.springbootjuniorcase.utils.HttpHeadersUtil;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +27,9 @@ public class CategoryController {
     private final HttpHeadersUtil headersUtil;
 
     @PostMapping
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Category created")
+    })
     public ResponseEntity<CategoryResponseDTO> create(@RequestBody CategoryRequestDTO  categoryRequestDTO) {
         var category = categoryService.create(categoryRequestDTO);
         var location = URI.create("/categories/" + category.getId());
@@ -33,6 +38,7 @@ public class CategoryController {
     }
 
     @GetMapping
+    @ApiResponse(responseCode = "200")
     public ResponseEntity<CategoryPaginationResponseDTO> getAll(@Valid @ModelAttribute CategoryPaginationRequestDTO pagination) {
         var category = categoryService.findAll(pagination);
         var headers = headersUtil.setPaginationHeaders(category);
@@ -41,6 +47,10 @@ public class CategoryController {
     }
 
     @GetMapping("/{id}/books")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Books found"),
+            @ApiResponse(responseCode = "404", description = "Category not found")
+    })
     public ResponseEntity<List<BookResponseDTO>> getBook(@PathVariable Long id) {
         return ResponseEntity.ok().body(categoryService.findBookByCategoryId(id));
     }
